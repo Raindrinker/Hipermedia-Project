58e61b79b2931a70efc9593808ea5dd501575401
Raindrinker <ferriruiz3@gmail.com>
2015-12-07 11:50:56 +0100

    Fixing the damn fixed top bar

--- js/sticky_top.js
+++ js/sticky_top.js
@@ -4,16 +4,19 @@ $(document).ready(function() {
     var bigtitle = document.getElementById('bigtitle');
     var smalltitle = document.getElementById('smalltitle');
 
-    console.log(bigtitle);
-
-    $(window).scroll(function() {
-        if(content.scrollTop > (bigtitle.clientHeight - smalltitle.clientHeight)){
+    var smallTitleControl = function() {
+        if (content.scrollTop > (bigtitle.clientHeight - smalltitle.clientHeight)) {
             smalltitle.style.visibility = "visible";
-            smalltitle.childNodes[1].style.opacity = (content.scrollTop - (bigtitle.clientHeight - smalltitle.clientHeight))/100;
-            console.log(bigtitle.offsetWidth);
-            smalltitle.style.width = bigtitle.offsetWidth + "px";
-        }else{
+            smalltitle.childNodes[1].style.opacity = (content.scrollTop - (bigtitle.clientHeight - smalltitle.clientHeight)) / 100;
+        } else {
             smalltitle.style.visibility = "hidden";
         }
-    });
+    }
+
+    console.log(bigtitle);
+
+    $(window).scroll(smallTitleControl);
+
+    $(window).resize(smallTitleControl);
+
 });
