75eb6ea54f74eaa066b8cc94ef790a2fff6d2260
Raindrinker <ferriruiz3@gmail.com>
2015-12-12 11:07:41 +0100

    Search. Menu & all the Handlebars

--- js/sticky_top.js
+++ js/sticky_top.js
@@ -3,15 +3,20 @@ $(document).ready(function() {
     var content = document.getElementsByTagName('body')[0];
     var bigtitle = document.getElementById('bigtitle');
     var smalltitle = document.getElementById('smalltitle');
+    var searchdiv = document.getElementById('searchdiv');
 
     var smallTitleControl = function() {
         if (content.scrollTop > (bigtitle.clientHeight - smalltitle.clientHeight)) {
             smalltitle.style.visibility = "visible";
-            bigtitle.style.visibility = "hidden";
+            searchdiv.style.position = "fixed";
+            searchdiv.style.top = "0px";
             smalltitle.childNodes[1].style.opacity = (content.scrollTop - (bigtitle.clientHeight - smalltitle.clientHeight)) / 100;
+
         } else {
             smalltitle.style.visibility = "hidden";
             bigtitle.style.visibility = "visible";
+            searchdiv.style.top = (130 - content.scrollTop) + "px";
+            searchdiv.style.position = "fixed";
         }
     }
 
