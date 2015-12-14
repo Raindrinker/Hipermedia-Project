01f640c206ef0ca751ba4e779fe1d27f0cea8980
Raindrinker <ferriruiz3@gmail.com>
2015-12-14 22:58:27 +0100

    Soft transition

--- js/main.js
+++ js/main.js
@@ -2,48 +2,57 @@ var api = new SpotifyWebApi();
 
 var app = new BetaPlayerApp(api, null);
 
-$("#searchbutton").click(function(event){
-  var query = $("#searcharea").val();
-  app.getSongsArtistsAlbumsFromName(query, 12, function(songs, artists, albums){
+$("#searchbutton").click(function(event) {
+    var query = $("#searcharea").val();
+    app.getSongsArtistsAlbumsFromName(query, 12, function(songs, artists, albums) {
 
-    // Grab the template script
-    var songsScript = $("#songs-template").html();
-    var artistsScript = $("#artists-template").html();
-    var albumsScript = $("#albums-template").html();
-
-
-    var songsElement = {
-      "song": songs
-    };
-
-    var artistsElement = {
-      "artist": artists
-    };
-
-    var albumsElement = {
-      "album": albums
-    };
-
-    // Compile the template
-    var artistsTemplate = Handlebars.compile(artistsScript);
-    var songsTemplate = Handlebars.compile(songsScript);
-    var albumsTemplate = Handlebars.compile(albumsScript);
+        // Grab the template script
+        var songsScript = $("#songs-template").html();
+        var artistsScript = $("#artists-template").html();
+        var albumsScript = $("#albums-template").html();
 
 
-    var compiledArtists = artistsTemplate(artistsElement);
-    var compiledAlbums = albumsTemplate(albumsElement);
-    var compiledSongs = songsTemplate(songsElement);
+        var songsElement = {
+            "song": songs
+        };
 
-    $(".base").empty();
-    if(artists.length > 0){
-        $(".base").append(compiledArtists);
-    }
-    if(albums.length > 0){
-        $(".base").append(compiledAlbums);
-    }
-    if(songs.length > 0){
-        $(".base").append(compiledSongs);
-    }
+        var artistsElement = {
+            "artist": artists
+        };
 
-  });
+        var albumsElement = {
+            "album": albums
+        };
+
+        // Compile the template
+        var artistsTemplate = Handlebars.compile(artistsScript);
+        var songsTemplate = Handlebars.compile(songsScript);
+        var albumsTemplate = Handlebars.compile(albumsScript);
+
+
+        var compiledArtists = artistsTemplate(artistsElement);
+        var compiledAlbums = albumsTemplate(albumsElement);
+        var compiledSongs = songsTemplate(songsElement);
+
+        $(".content-overlay").css("opacity", "0");
+        $(".content-overlay").css("pointer-events", "none");
+        setTimeout(function() {
+            $(".base").empty();
+            if (artists.length > 0) {
+                $(".base").append(compiledArtists);
+            }
+            if (albums.length > 0) {
+                $(".base").append(compiledAlbums);
+            }
+            if (songs.length > 0) {
+                $(".base").append(compiledSongs);
+            }
+            setTimeout(function() {
+                $(".content-overlay").css("opacity", "1");
+                $(".content-overlay").css("pointer-events", "all");
+            }, 250);
+        }, 250);
+
+
+    });
 });
