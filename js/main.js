90d48c7ac50911c1f888a6569f0a03bf5404f55f
Albert Llaurado <albert.llaurado@hotmail.com>
2015-12-17 20:08:57 +0100

    Artist Layout in progress

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var app = new BetaPlayerApp(api, null);
 var youtubeApi = new YoutubeApi(YOUTUBE_TOKEN);
 var youtubeMP3Api = new YoutubeMP3();
 
-$("#search_form").submit(function(event){
+$("#search_form").submit(function(event) {
   event.preventDefault();
   var query = $("#searcharea").val();
 
@@ -20,7 +20,7 @@ $("#search_form").submit(function(event){
       });
   });*/
 
-  app.getSongsArtistsAlbumsFromName(query, 12, function(songs, artists, albums){
+  app.getSongsArtistsAlbumsFromName(query, 12, function(songs, artists, albums) {
 
     // Grab the template script
     var songsScript = $("#songs-template").html();
@@ -50,69 +50,117 @@ $("#search_form").submit(function(event){
 
     //Afegim el listener per tots els elements amb class="square":
 
-        $(".content-overlay").css("opacity", "0");
-        $(".content-overlay").css("pointer-events", "none");
-        setTimeout(function() {
+    $(".content-overlay").css("opacity", "0");
+    $(".content-overlay").css("pointer-events", "none");
+    setTimeout(function() {
+      $(".base").empty();
+      if (artists.length > 0) {
+        $(".base").append(compiledArtists);
+
+        $(".square").map(function(index, item) {
+          $(item).click(function(event) {
+
+            console.log("CLICKED: " + item.dataset.groupid);
+
             $(".base").empty();
-            if (artists.length > 0) {
-                $(".base").append(compiledArtists);
 
-                $(".square").map(function(index, item){
-                  $(item).click(function(event){
+            /*
+              JSON PER L'ALBERT:
 
-                    console.log("CLICKED: " + item.dataset.groupid);
-
-                    $(".base").empty();
-
-                    /*
-                      JSON PER L'ALBERT:
-
-                        {
-                            "groupId": 12345,
-                            "artistName": "Queen",
-                            "imgRoute": "rutaImgQueen",
-                            "biography": "Vida y milagros",
-                            "topSongs":[
+                {
+                    "groupId": 12345,
+                    "artistName": "Queen",
+                    "imgRoute": "rutaImgQueen",
+                    "biography": "Vida y milagros",
+                    "topSongs":[
+                              {
+                                "songId":12345,
+                                "songName":"NomCanço",
+                                "albumName":"NomAlbum",
+                                "imgRoute":"rutaCanço"
+                              },
+                              {
+                                "songId":12345,
+                                "songName":"NomCanço",
+                                "albumName":"NomAlbum",
+                                "imgRoute":"rutaCanço"
+                              }
+                            ]
+                            "albums":[
                                       {
-                                        "songId":12345,
-                                        "songName":"NomCanço",
+                                        "albumId":12345,
                                         "albumName":"NomAlbum",
-                                        "imgRoute":"rutaCanço"
+                                        "imgRoute":"rutaAlbum"
                                       },
                                       {
-                                        "songId":12345,
-                                        "songName":"NomCanço",
+                                        "albumId":12345,
                                         "albumName":"NomAlbum",
-                                        "imgRoute":"rutaCanço"
+                                        "imgRoute":"rutaAlbum"
                                       }
                                     ]
-                        }  
-                    */
+                }
+            */
 
-                    var artistMainAreaScript = $("#artist-main-template").html();
-                    console.log(artistMainAreaScript);
-                    var artistMainAreaTemplate = Handlebars.compile(artistMainAreaScript);
-                    var artistMainAreaElement = {
-                      "artist": artist
-                    };
-                    var compiledArtistMainArea = artistMainAreaTemplate(artistMainAreaElement);
+            var artistMainAreaScript = $("#artist-main-template").html();
+            /*var artistMainAreaTemplate = Handlebars.compile(artistMainAreaScript;);
+            var artistMainAreaElement = {
+              "artist": artist
+            };
+            var compiledArtistMainArea = artistMainAreaTemplate(artistMainAreaElement);
 
-                    $(".base").append(compiledArtistMainArea);
+            $(".base").append(compiledArtistMainArea);*/
+            $(".base").append(artistMainAreaScript);
 
-                  });
-                });
-            }
-            if (albums.length > 0) {
-                $(".base").append(compiledAlbums);
-            }
-            if (songs.length > 0) {
+            var sliderHeight = "100px";
+            $('.panel-biography').each(function() {
+                var current = $(this);
+                current.attr("box_h", current.height());
+              }
+
+            );
+
+            $(".panel-biography").css("height", sliderHeight);
+            $(".toggle-biography").html('<a href="#">click</a>');
+            $(".toggle-biography a").click(function() {
+              openSlider()
+            })
+
+            /*if (artist.topSongs.length > 0) {
                 $(".base").append(compiledSongs);
-            }
-            setTimeout(function() {
-                $(".content-overlay").css("opacity", "1");
-                $(".content-overlay").css("pointer-events", "all");
-            }, 250);
-        }, 250);
+            }*/
+
+            /*if (artist.albums.length > 0) {
+                $(".base").append(compiledAlbums);
+            }*/
+
+          });
+        });
+      }
+      if (albums.length > 0) {
+        $(".base").append(compiledAlbums);
+      }
+      if (songs.length > 0) {
+        $(".base").append(compiledSongs);
+      }
+      setTimeout(function() {
+        $(".content-overlay").css("opacity", "1");
+        $(".content-overlay").css("pointer-events", "all");
+      }, 250);
+    }, 250);
 
   });
 });
+
+function openSlider(){
+    var open_height = $(".panel-biography").attr("box_h") + "px";
+    $(".panel-biography").animate({"height": open_height}, {duration: "slow" });
+    $(".toggle-biography").html('<a href="#">close</a>');
+    $(".toggle-biography a").click(function() { closeSlider() })
+}
+
+function closeSlider(){
+    var sliderHeight = "100px";
+    $(".panel-biography").animate({"height": sliderHeight}, {duration: "slow" });
+    $(".toggle-biography").html('<a href="#">click</a>');
+    $(".toggle-biography a").click(function() { openSlider() })
+}
